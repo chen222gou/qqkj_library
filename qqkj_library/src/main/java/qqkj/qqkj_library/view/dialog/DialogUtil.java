a1f4b44f7446e9549ceb724a3e5ca12bd05a3f63
apple <948262850@qq.com>
2017-11-08 19:17:41 +0800

    新增Dialog

---
+++ qqkj_library/src/main/java/qqkj/qqkj_library/view/dialog/DialogUtil.java
@@ -0,0 +1,81 @@
+package qqkj.qqkj_library.view.dialog;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+
+/**
+ * 这个类是用来做甚的
+ * <p>
+ * Created by 陈二狗 on 2017/11/8.
+ */
+
+public class DialogUtil {
+
+    private Context _context = null;
+
+    public DialogUtil(Context _context) {
+
+        this._context = _context;
+    }
+
+
+    public static DialogUtil getIns(Context _context) {
+
+        return new DialogUtil(_context);
+    }
+
+
+    /**
+     * 2个按钮(确定,取消)
+     *
+     * @param _title
+     * @param _msg
+     * @param _sure_text
+     * @param _cancel_text
+     * @param _sure_click
+     * @param _cancel_click
+     */
+    public AlertDialog _get_dialog_two_btn(String _title, String _msg, String _sure_text, String _cancel_text,
+
+                                           DialogInterface.OnClickListener _sure_click, DialogInterface.OnClickListener _cancel_click) {
+
+        AlertDialog.Builder _dialog_builder = new AlertDialog.Builder(_context);
+
+        _dialog_builder.setTitle(_title);
+
+        _dialog_builder.setMessage(_msg);
+
+        _dialog_builder.setPositiveButton(_sure_text, _sure_click);
+
+        _dialog_builder.setNegativeButton(_cancel_text, _cancel_click);
+
+        _dialog_builder.setCancelable(false);
+
+        return _dialog_builder.show();
+    }
+
+
+    /**
+     * 一个按钮(确定)
+     *
+     * @param _title
+     * @param _msg
+     * @param _sure_text
+     * @param _sure_click
+     */
+    public AlertDialog _get_dialog_one_btn(String _title, String _msg, String _sure_text, DialogInterface.OnClickListener _sure_click) {
+
+        AlertDialog.Builder _dialog_builder = new AlertDialog.Builder(_context);
+
+        _dialog_builder.setTitle(_title);
+
+        _dialog_builder.setMessage(_msg);
+
+        _dialog_builder.setPositiveButton(_sure_text, _sure_click);
+
+        _dialog_builder.setCancelable(false);
+
+        return _dialog_builder.show();
+    }
+}
