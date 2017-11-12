5fd5f662aef6e44fbe68c64a80ecdc785503f69c
apple <948262850@qq.com>
2017-11-12 17:38:33 +0800

    新增安装Apk文件

---
+++ qqkj_library/src/main/java/qqkj/qqkj_library/app/install/InstallUtil.java
@@ -0,0 +1,53 @@
+package qqkj.qqkj_library.app.install;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+import java.io.File;
+
+/**
+ * 这个类是用来做甚的
+ * <p>
+ * Created by 陈二狗 on 2017/11/10.
+ */
+
+public class InstallUtil {
+
+    private static InstallUtil _install_util = null;
+
+    private Intent _intent = null;
+
+    private Context _context = null;
+
+
+    public InstallUtil(Context _context){
+
+        this._context = _context;
+    }
+
+
+    public static InstallUtil getIns(Context _context) {
+
+        if (_install_util == null) {
+
+            _install_util = new InstallUtil(_context);
+        }
+
+        return _install_util;
+    }
+
+
+    /**
+     * 安装APK
+     * @param _apk_path    APK文件存放路径
+     */
+    public void _get_install_apk(String _apk_path){
+
+        _intent = new Intent(Intent.ACTION_VIEW);
+
+        _intent.setDataAndType(Uri.fromFile(new File(_apk_path)),"application/vnd.android.package-archive");
+
+        _context.startActivity(_intent);
+    }
+}
